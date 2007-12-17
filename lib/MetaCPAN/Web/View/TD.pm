765da9527aa76b331350aa2748566a43b0071a85
Jonathan Rockway <jon@jrock.us>
2007-12-16 19:16:12 -0600

    add TD view

---
+++ lib/MetaCPAN/Web/View/TD.pm
@@ -0,0 +1,10 @@
+package MetaCPAN::Web::View::TD;
+use strict;
+use warnings;
+use base 'Catalyst::View::Template::Declare';
+1;
+
+# Put templates in separate modules, each under the 
+# MetaCPAN::Web::View::TD::* namespace.  Be sure to "use Template::Declare::Tags"
+# in each one.
+
