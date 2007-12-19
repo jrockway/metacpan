a23ef1b9b047461dee61a744e95fc03bdfb2e60f
Jonathan Rockway <jon@jrock.us>
2007-12-19 02:28:21 -0600

    add a MetaCPAN.pm describing the project

---
+++ lib/MetaCPAN.pm
@@ -0,0 +1,19 @@
+package MetaCPAN;
+use v5.10.0;
+our $VERSION = '0.01';
+1;
+
+__END__
+
+=head1 NAME
+
+MetaCPAN - project to make CPAN metadata easily available
+
+=head1 AUTHOR
+
+Jonathan Rockway C<< <jrockway@cpan.org> >
+
+=head1 LICENSE
+
+This program is free software; you may redistribute it under the same
+terms as Perl itself.
